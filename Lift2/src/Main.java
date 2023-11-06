b38c6a5eb73ae2fe414b14dedcd77f63cf8b5e34
domipiggie <basti.domonkos.2020i@bankitatabanya.hu>
2023-11-06 10:29:17 +0100

    lift igen

--- Lift2/src/Main.java
+++ Lift2/src/Main.java
@@ -26,8 +26,42 @@ public class Main {
             throw new RuntimeException(e);
         }
 
+        System.out.println("\n1. Feladat");
+        System.out.println("„A lift a(z) " + igenyek.get(igenyek.size()-1).getDestinationFloor() + ". szinten áll az utolsó igény teljesítése után.");
+
+        System.out.println("\n2. Feladat");
+        int minLevel = igenyek.get(0).getStartFloor();
+        int maxLevel = igenyek.get(0).getStartFloor();
+
         for (int i = 0; i < igenyek.size(); i++){
-            System.out.println(igenyek.get(i));
+            if (igenyek.get(i).getDestinationFloor() > maxLevel){
+                maxLevel = igenyek.get(i).getDestinationFloor();
+            }
+            if (igenyek.get(i).getDestinationFloor() < minLevel){
+                minLevel = igenyek.get(i).getDestinationFloor();
+            }
         }
+
+        System.out.println("A legmagasabb érintett szint: " + maxLevel);
+        System.out.println("A legkisebb érintett szint: " + minLevel);
+
+        System.out.println("\n3. Feladat");
+        int upWithPassengers = 0;
+        int upWithoutPassengers = 0;
+
+        for (int i = 1; i < igenyek.size(); i++){
+            Igeny ig = igenyek.get(i);
+            Igeny igMinus = igenyek.get(i-1);
+
+            if (ig.getStartFloor() > igMinus.getDestinationFloor()){
+                upWithoutPassengers++;
+            }
+            if (ig.getDestinationFloor() > ig.getStartFloor()){
+                upWithPassengers++;
+            }
+        }
+
+        System.out.println("A lift " + upWithoutPassengers + " alkalommal indult felfelé utasok nélkül.");
+        System.out.println("A lift " + upWithPassengers + " alkalommal indult felfelé utasokkal.");
     }
 }
